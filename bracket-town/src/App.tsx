40deb86008c8928da4e0c129de8e29ee39446b34
Amir Rave <>
2025-04-01 22:14:23 +0300

    score

--- bracket-town/src/App.tsx
+++ bracket-town/src/App.tsx
@@ -5,6 +5,8 @@ import Toast from './Toast';
 
 function App() {
   const [state, setState] = useState({ value: "" });
+  const [score, setScore] = useState(100);
+
   const puzzleRef = useRef<any>(null);
   const toastRef = useRef<any>(null);
 
@@ -16,7 +18,7 @@ function App() {
     const result = puzzleRef.current.submitAnswer(state.value);
     // console.log(result);
     if (result === false) {
-      // TODO subtract score
+      setScore((score) => score - 2);
       toastRef.current.showError('טעות! הניחוש לא תואם אף אחד מהסוגרים.')
     }
 
@@ -25,7 +27,7 @@ function App() {
   }
 
   const onRequestHint = () => {
-    // TODO subtract score
+    setScore((score) => score - 15);
     return true;
   }
 
@@ -38,12 +40,16 @@ function App() {
         </div>
         {/* <p>{state.value}</p> */}
         <div className='puzzle-content'>
-        <Puzzle ref={puzzleRef} requestHint={onRequestHint} puzzleKey='[דרך|___ א[גב|לתקוע סכין במקום זה משמעותו לבגוד]] א[רץ|"יונתן הקטן __ ב[בוקר|שדה ___, מגוריו של הנ[שיא|רשומה בספר גינס, לדוגמה] הראשון בערוב ימיו] אל ה[גן|מילה שבאה לפני "חיות" ו"שעשועים"]"] [קד|השת[חווה|אמם של קין ואבל] בפני]מה ל[תור|קשה לקבוע אחד [כזה|ככה וככה ו-___ ו-___ (אושר כהן)] ל[רופא|בעבר הרחוק אחד היה מטפל בך באמצעות הקזת דם]]ה'></Puzzle>
-        <Toast ref={toastRef}></Toast>
-        <form className='input-container' onSubmit={onSubmit}>
-          <input className='puzzle-input' type='text' name='answer' placeholder='הקלד את הניחוש שלך...' value={state.value} onChange={onInputChange} />
-          <button className='puzzle-submit' disabled={(state.value ? false : true)}>שלח</button>
-        </form>
+          <div className='puzzle-score'>
+            <label>ניקוד: {score}/100</label>
+            <progress value={score} max='100'>{score}%</progress>
+          </div>
+          <Puzzle ref={puzzleRef} requestHint={onRequestHint} puzzleKey='[דרך|___ א[גב|לתקוע סכין במקום זה משמעותו לבגוד]] א[רץ|"יונתן הקטן __ ב[בוקר|שדה ___, מגוריו של הנ[שיא|רשומה בספר גינס, לדוגמה] הראשון בערוב ימיו] אל ה[גן|מילה שבאה לפני "חיות" ו"שעשועים"]"] [קד|השת[חווה|אמם של קין ואבל] בפני]מה ל[תור|קשה לקבוע אחד [כזה|ככה וככה ו-___ ו-___ (אושר כהן)] ל[רופא|בעבר הרחוק אחד היה מטפל בך באמצעות הקזת דם]]ה'></Puzzle>
+          <Toast ref={toastRef}></Toast>
+          <form className='input-container' onSubmit={onSubmit}>
+            <input className='puzzle-input' type='text' name='answer' placeholder='הקלד את הניחוש שלך...' value={state.value} onChange={onInputChange} />
+            <button className='puzzle-submit' disabled={(state.value ? false : true)}>שלח</button>
+          </form>
         </div>
       </div>
     </>
